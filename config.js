971a80c4fc2ae7881dcea25d0fe56b6042409c0f
anuhasnimasara <110512761+anuhasnimasara@users.noreply.github.com>
2022-08-19 19:43:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,20 +18,20 @@ global.APIKeys = {
 
 //Owner React
 global.OWNER_REACT = '🧑🏻‍💻' //මෙතනට emoji එකක දාන්න.
-global.REACT_NUMBER = '9476342733' //මෙතන number එක.
+global.REACT_NUMBER = '94783745231' //මෙතන number එක.
 
 // Other
-global.owner = ['9476342733',''] //මෙතන number එක.
-global.premium = ['9476342733','9476342733'] //මෙතන number එක.
-global.botname = 'VIHANGA BOT INC' //මෙතන නම.
-global.ownername = 'Vihanga MD' //මෙතන නම.
-global.ownername = 'ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ' //මෙතන නම.
-global.group1 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.group2 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.packname = 'VIHANGA BOT INC' //මෙතන නම.
-global.cap = '*By Vihanga MD*' //මෙතන නම.
-global.img = 'https://i.ibb.co/BBB6Rxn/Vihanga-MD.png' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
-global.ytchannel = 'https://youtu.be/Z9rIR0Vopb8' //මෙතන කැමති ලින්ක් එකක්.
+global.owner = ['94783745231',''] //මෙතන number එක.
+global.premium = ['94783745231','94783745231'] //මෙතන number එක.
+global.botname = '꧁ᴹᴿ ᭄𝙰𝙽𝚄𝚆𝙰 𝙼𝙾𝙳𝚂 ༒࿐' //මෙතන නම.
+global.ownername = '꧁ᴹᴿ ᭄𝙰𝙽𝚄𝚆𝙰 𝙼𝙾𝙳𝚂 ༒࿐' //මෙතන නම.
+global.ownername = '𝘼𝙉𝙐𝙃𝘼𝙎 ʏᴛ' //මෙතන නම.
+global.group1 = 'https://chat.whatsapp.com/JnDFkTygUApHDlKPG1XssD' //මෙතන grouplink එක.
+global.group2 = 'https://chat.whatsapp.com/JnDFkTygUApHDlKPG1XssD' //මෙතන grouplink එක.
+global.packname = '꧁ᴹᴿ ᭄𝙰𝙽𝚄𝚆𝙰 𝙼𝙾𝙳𝚂 ༒࿐' //මෙතන නම.
+global.cap = '*By 𝘼𝙉𝙐𝙃𝘼𝙎 MD*' //මෙතන නම.
+global.img = 'https://i.imgur.com/eb58dLG.jpeg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
+global.ytchannel = 'https://youtube.com/channel/UC8Lgyty47CU-uORJpsFV70Q' //මෙතන කැමති ලින්ක් එකක්.
 global.monayawal = '100000' //මෙතන මාරු කරන්න එපා.
 global.darahawal = '100' //මෙතන මාරු කරන්න එපා.
 global.besiAwal = '15' //මෙතන මාරු කරන්න එපා.
@@ -52,7 +52,7 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip' //මෙතන මාරු කරන්න එපා.
 global.f5 = 'application/pdf' //මෙතන මාරු කරන්න එපා.
 global.f6 = 'application/vnd.android.package-archive' //මෙතන මාරු කරන්න එපා.
-global.author = 'VIHANGA BOT INC' //මෙතන නම.
+global.author = '꧁ᴹᴿ ᭄𝙰𝙽𝚄𝚆𝙰 𝙼𝙾𝙳𝚂 ༒࿐' //මෙතන නම.
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=' //මෙතන මාරු කරන්න එපා.
 global.sessionName = 'session' //මෙතන මාරු කරන්න එපා.
 global.prefa = ['','!','.','🐦','🐤','🗿'] //මෙතන මාරු කරන්න එපා.
